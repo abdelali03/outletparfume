81175b90388ad9f41a725c71249c0bf7abd1509e
Your Name <you@example.com>
2024-08-21 23:39:14 +0100

    broken our homepage into smaller components, finished styling our menu-item component

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./homepage.style.scss";
+import Directory from "../../components/directoy/directory.component";
+
+const Homepage = () => (
+  <div className="homepage">
+    <Directory />
+  </div>
+);
+export default Homepage;
