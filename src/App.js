f973c18717dce996a4c575d60fd264eeca3ddf39
Your Name <you@example.com>
2024-08-22 18:06:10 +0100

    update our menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
-
+import { Switch, Route } from "react-router-dom";
 import Homepage from "./pages/homepage/homepage.component";
+const HastPage = () => (
+  <div>
+    <h1>hats page</h1>
+  </div>
+);
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route exact path="/hats" component={HastPage} />
+      </Switch>
     </div>
   );
 }
