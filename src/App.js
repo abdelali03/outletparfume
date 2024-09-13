736e5c07847105b0e9f4cbd653fe78c990e38d98
Your Name <you@example.com>
2024-09-14 00:15:29 +0100

    finishing sign in handleSubmit method

--- src/App.js
+++ src/App.js
@@ -33,7 +33,6 @@ class App extends React.Component {
       } else {
         this.setState({ currentUser: userAuth });
       }
-      console.log(this.state);
     });
   }
 
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import "./sign-in.style.scss";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
+import { signInWithEmailAndPassword } from "firebase/auth"; // Import for Firebase v9
+
 class SignIn extends React.Component {
   constructor(props) {
     super(props);
@@ -12,9 +14,17 @@ class SignIn extends React.Component {
     };
   }
 
-  handlesubmit = (event) => {
+  handlesubmit = async (event) => {
     event.preventDefault();
-    this.setState({ email: "", password: "" });
+    const { email, password } = this.state;
+
+    try {
+      // Use Firebase v9 syntax
+      await signInWithEmailAndPassword(auth, email, password);
+      this.setState({ email: "", password: "" });
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   handleChange = (event) => {
@@ -25,7 +35,7 @@ class SignIn extends React.Component {
   render() {
     return (
       <div className="sign-in">
-        <h2> I already have an account</h2>
+        <h2>I already have an account</h2>
         <span>Sign in with your email and password</span>
 
         <form onSubmit={this.handlesubmit}>
@@ -47,10 +57,9 @@ class SignIn extends React.Component {
             handleChange={this.handleChange}
           />
           <div className="buttons">
-            <CustomButton type="submit">Sign in </CustomButton>
+            <CustomButton type="submit">Sign in</CustomButton>
             <CustomButton isGoogleSignIn onClick={signInWithGoogle}>
-              {" "}
-              Sign in with Google{" "}
+              Sign in with Google
             </CustomButton>
           </div>
         </form>
